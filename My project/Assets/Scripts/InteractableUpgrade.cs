3551fa04ea32f3dc21ae3dd8ed3a1fab326cf95d
AmandinCR <amandinchyba@gmail.com>
2024-07-23 02:11:09 -0400

    smoother fov change i think?

--- My project/Assets/Scripts/InteractableUpgrade.cs
+++ My project/Assets/Scripts/InteractableUpgrade.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Mirror;
-using UnityEditor.UI;
 using UnityEngine;
 
 public class InteractableUpgrade : MonoBehaviour
--- My project/Assets/Scripts/PlayerFOV.cs
+++ My project/Assets/Scripts/PlayerFOV.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 using KinematicCharacterController;
 using KinematicCharacterController.Examples;
 using DG.Tweening;
+using Steamworks;
 
 public class PlayerFOV : NetworkBehaviour
 {
     private Camera cam;
     private KinematicCharacterMotor characterMotor;
-    [SerializeField] private float velocityFOVChange;
-    [SerializeField] private float fastFOV;
-    [SerializeField] private float slowFOV;
+    [SerializeField] private float maxVelocityFOV;
+    [SerializeField] private float minVelocityFOV;
+    [SerializeField] private float maxFOVChange;
+    [SerializeField] private float baseFOV;
     [SerializeField] private float fovTransitionSpeed;
-    private float fovChangeTimer = 0f;
-    private bool goingFast = false;
+    private float targetFOV;
 
     private void Start()
     {
@@ -30,43 +31,43 @@ public class PlayerFOV : NetworkBehaviour
 
     private void Update()
     {
+        // don't change fov in the air, it feels weird
+        if (!characterMotor.GroundingStatus.IsStableOnGround && cam.fieldOfView == baseFOV + maxFOVChange) 
+        {
+            return;
+        }
+
         Vector3 currentVel = characterMotor.BaseVelocity;
         currentVel.y = 0f;
-        if (!goingFast)
+        targetFOV = baseFOV;
+        if (currentVel.magnitude > maxVelocityFOV)
         {
-            if (currentVel.magnitude >= velocityFOVChange)
-            {
-                if (cam.fieldOfView != fastFOV)
-                {
-                    DOFov(fastFOV);
-                    goingFast = true;
-                }
-            }
+            targetFOV = baseFOV + maxFOVChange;
         }
 
-        if (fovChangeTimer <= 0f)
-        {
-            if (goingFast)
-            {
-                if (currentVel.magnitude < velocityFOVChange)
-                {
-                    if (cam.fieldOfView != slowFOV)
-                    {
-                        DOFov(slowFOV);
-                        goingFast = false;
-                    }
-                }
-            }
-        }
+        /* for continuous fov changes per velocity
+        if (currentVel.magnitude < minVelocityFOV) 
+            targetFOV = baseFOV;
         else
-        {
-            fovChangeTimer -= Time.deltaTime;
-        }
-    }
+            targetFOV =  baseFOV + maxFOVChange * Mathf.Min(currentVel.magnitude / maxVelocityFOV, 1);
+        */
 
-    private void DOFov(float newFOV)
-    {
-        fovChangeTimer = fovTransitionSpeed;
-        cam.DOFieldOfView(newFOV, fovTransitionSpeed);
+        if (targetFOV > cam.fieldOfView)
+        {
+            cam.fieldOfView += Time.deltaTime*fovTransitionSpeed;
+            if (cam.fieldOfView > targetFOV) 
+            {
+                cam.fieldOfView = targetFOV;
+            }
+        }
+        else if (targetFOV < cam.fieldOfView)
+        {
+            cam.fieldOfView -= Time.deltaTime*fovTransitionSpeed;
+            if (cam.fieldOfView < targetFOV) 
+            {
+                cam.fieldOfView = targetFOV;
+            }
+        }
+        
     }
 }
